f341a5b04c91844a637e6e497bee83b7a1259dca
christina <781288772@qq.com>
2022-08-01 11:52:28 +0800

    上传db文件

---
+++ config/db.ts
@@ -0,0 +1,18 @@
+export default {
+    mysql: {
+        host: 'localhost',
+        port: 3306,
+        database: 'nestjs2',
+        user: 'root',
+        password: '123456',
+        connectionLimit: 10,
+    },
+    // mysql: {
+    //     host: 'localhost',
+    //     port: 3306,
+    //     database: 'nestjs2',
+    //     user: 'nestjs2',
+    //     password: 'fxxaW4aH4TENbanc',
+    //     connectionLimit: 10,
+    // },
+}
