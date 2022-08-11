26069d2bb61d57d376de6b768825a873d6701282
christina <781288772@qq.com>
2022-08-11 11:07:27 +0800

    feat:调整bannerDTO :rocket:

--- src/logical/banner/dto/create-banner.dto.ts
+++ src/logical/banner/dto/create-banner.dto.ts
@@ -2,15 +2,18 @@ import { Column } from "sequelize-typescript";
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { ApiProperty } from "@nestjs/swagger";
 export class CreateBannerDto {
+    // id
  id: number;
 
  
+//  图片路径
  @ApiProperty()
  @IsNotEmpty({ message: '图片不能为空' })
-src:string;
+ src:string;
 
+ //创建时间
  create_time:string;
-
+// 更新时间
  update_time:string;
 
  is_hidden:boolean;
