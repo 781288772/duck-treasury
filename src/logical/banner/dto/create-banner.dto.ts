f85e3c8b7518cec4055214bb11c9f8045c700c30
christina <781288772@qq.com>
2022-08-15 16:05:36 +0800

    fix:修复

--- src/logical/banner/dto/create-banner.dto.ts
+++ src/logical/banner/dto/create-banner.dto.ts
@@ -4,17 +4,13 @@ import { ApiProperty } from "@nestjs/swagger";
 export class CreateBannerDto {
     // id
  id: number;
-
- 
 //  图片路径
  @ApiProperty()
  @IsNotEmpty({ message: '图片不能为空' })
  src:string;
-
  //创建时间
  create_time:string;
 // 更新时间
  update_time:string;
-
  is_hidden:boolean;
 }
